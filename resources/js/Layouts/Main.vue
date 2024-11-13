da758e75f8e4daa1ccdecc8adb38cd4aeedfbb88
Kreoin <steamuser85@yandex.ru>
2024-11-13 21:37:25 +0300

    fix

---
+++ resources/js/Layouts/Main.vue
@@ -0,0 +1,19 @@
+<script setup></script>
+
+<template>
+    <div>
+        <header class="bg-blue-800 text-white">
+            <nav
+                class="p-6 mx-auto max-w-screen-lg flex items-center justify-between"
+            >
+                <div class="space-x-6">
+                    <Link :href="route('home')">Home</Link>
+                </div>
+            </nav>
+        </header>
+
+        <main class="p-6 mx-auto max-w-screen-lg">
+            <slot />
+        </main>
+    </div>
+</template>
